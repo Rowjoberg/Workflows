722070b54d6b92f51c10e76fb38504ebae4bf746
Rowan Fromberg <169016910+Rowjoberg@users.noreply.github.com>
2025-04-22 09:09:23 +1000

    Added Ampcontrol workflows

--- Powershell/PS-Scripts.ps1
+++ Powershell/PS-Scripts.ps1
@@ -1,9 +1,56 @@
 # Pulled from $PROFILE
 
-function chkfont(){ # Returns all the fonts installed on the system
-    return (New-Object System.Drawing.Text.InstalledFontCollection).Families
+function chkfont(){
+  # Returns all the fonts installed on the system
+  return (New-Object System.Drawing.Text.InstalledFontCollection).Families
+}
+function Test-Script-Help(){
+  $tshelp = "When updating the config files, run the following commands:
+        progdatabase . # in an updated http://subversion/svn/csm/tools/AmpTest/config
+        progression --database .\prog_config.zip # To test the database
+    Commit all modified files int the config folder to svn."
+  return Write-Output $tshelp
+}
+
+function Find-Part-Certs ($partnum, $pypath="C:\wc\sytelineapps\src\syteline\find_item_certifications.py", $outputpath="C:\wc\AIRs\$partnum"+"_1"){
+  if ($null -eq $partnum){
+    return Write-Output "Please provide a Syteline part number"
+  } else {
+    # pip install -r "C:\wc\sytelineapps\requirements-test.txt"
+    py "$pypath" -p "$partnum" -o "$outputpath"
+    Set-Location "$outputpath"
+    Invoke-Item "$outputpath"
+    return 
+  }    
+}
+
+function AmpTestDevSetup ($partnum, $module){
+  if ($null -eq $partnum){
+    return Write-Output "Please provide a part number"
+  }
+  if ($null -eq $module){
+    return Write-Output "Please provide a module e.g. board, final, etc."
+  }
+  atdev debug "$partnum-$module"
+  Write-Output {Run the following commands:
+    .venv\Scripts\activate
+    py atdev_main.py # This will run the 'IPM3 Capacitor Bank Test' test script. 
+    # atdev --help
+  }
 }
 
 function Show-Path-Env(){
-    return $env:Path -replace ';', "`n"
+  return $env:Path -replace ';', "`n"
+}
+
+function BOMCompare ($bom1, $bom2){
+  if ($null -eq $bom1){
+    return Write-Output "Please provide a path to the first BOM"
+  }
+  if ($null -eq $bom2){
+    return Write-Output "Please provide a path to the second BOM"
+  }
+  # python -m pip install -r "C:\wc\BOM Compare\requirements.txt"
+  python "C:\wc\BOM Compare\BOMCompare.py" $bom1 $bom2
+    
 }
--- Powershell/PS-Variables.ps1
+++ Powershell/PS-Variables.ps1
@@ -1,4 +1,6 @@
-$WC = 'C:\wc\'
+$AIR = "G:\Purchasing\Product Support Issues\Alternate Item Requests\Alternate Item Data\"
+$WC = "C:\wc\"
+$AMPTESTLOGS = "G:\Production\Test Programs\Amptest Logs\"
 $WORKFLOWS = 'C:\wc\WORKFLOWS\'
 $NVIM = '~\AppData\Local\nvim'
 $WEZTERM = '~\.config\wezterm\'
