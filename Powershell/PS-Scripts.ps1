53fc4fa363b11b47995ff8b978612a8140b47e2e
Rowjoberg <Rowan.fromberg@gmail.com>
2025-11-23 15:46:25 +1100

    Added yazi support

--- Powershell/PS-Scripts.ps1
+++ Powershell/PS-Scripts.ps1
@@ -1,4 +1,15 @@
 # Pulled from $PROFILE
+#
+# yazi Shell Function
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
 
 function Show-Fonts() {
   # Returns all the fonts installed on the system
