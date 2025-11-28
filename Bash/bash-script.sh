115ad6da1e14e7a6cecf579654ffc80f885537c5
Server Rowan <rowan.fromberg@gmail.com>
2025-11-28 23:13:59 +1100

    Added bash script import

---
+++ Bash/bash-script.sh
@@ -0,0 +1,29 @@
+# Ran from .bashrc
+
+[[ $- != *i* ]] && return
+
+bind 'TAB:menu-complete'
+bind 'set show-all-if-ambiguous on'
+#
+# Set up fzf key bindings and fuzzy completion
+eval "$(fzf --bash)"
+alias lg=lazygit
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
+
+#===BAT===#
+export BAT_THEME="Catppuccin Mocha"
+#===Yazi===#
+export EDITOR=nvim
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+#===Oh My Posh===#
+export PATH=$PATH:/home/rowan/.local/bin
+eval "$(oh-my-posh init bash --config "~/Workflows/Themes/catppuccinMocha.omp.json")"
