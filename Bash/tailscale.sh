2a6ead30b3700ae8eb72ff0fde1d32d3240ef008
Server Rowan <rowan.fromberg@gmail.com>
2026-02-20 20:46:41 +1100

    Added tailscale scripts

---
+++ Bash/tailscale.sh
@@ -0,0 +1,5 @@
+sudo tailscale update
+sudo tailscale up
+sudo tailscale serve set-config --all ~/Workflows/Bash/tailscale.sh
+# tailscale exit-node suggest
+# sudo tailscale set --exit-node=au-syd-wg-103.mullvad.ts.net. --exit-node-allow-lan-access=true
